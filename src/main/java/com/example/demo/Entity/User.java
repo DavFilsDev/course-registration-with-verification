40c07fb3d7d9870c0b08b637590442b84c4e46b5
DavFilsDev <miharisoadavidfils@gmail.com>
2026-07-04 23:41:16 +0300

    refactor(user): add all-args constructor for testing

--- src/main/java/com/example/demo/Entity/User.java
+++ src/main/java/com/example/demo/Entity/User.java
@@ -2,6 +2,7 @@ package com.example.demo.Entity;
 
 import jakarta.persistence.*;
 import java.util.UUID;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "users")
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class User {
   @Id @GeneratedValue private UUID id;
   private String firstName;
