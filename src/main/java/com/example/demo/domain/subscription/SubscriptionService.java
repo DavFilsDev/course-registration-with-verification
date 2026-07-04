0e50bfa3868a1e093cb0574973422cfa24c862dc
DavFilsDev <miharisoadavidfils@gmail.com>
2026-07-04 22:52:09 +0300

    refactor(repository): move User class in the repository package

--- src/main/java/com/example/demo/domain/subscription/SubscriptionService.java
+++ src/main/java/com/example/demo/domain/subscription/SubscriptionService.java
@@ -2,7 +2,7 @@ package com.example.demo.domain.subscription;
 
 import com.example.demo.Entity.Subscription;
 import com.example.demo.domain.course.CourseRepository;
-import com.example.demo.domain.user.UserRepository;
+import com.example.demo.repository.UserRepository;
 import com.example.demo.mail.Email;
 import com.example.demo.mail.Mailer;
 import jakarta.mail.internet.AddressException;
